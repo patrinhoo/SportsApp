c863e1fa462790df10f2c5ed908cf026d89c3b5d
patrinhoo <81069467+patrinhoo@users.noreply.github.com>
2023-01-26 15:04:53 +0100

    Deployment

--- my-app/src/conf.js
+++ my-app/src/conf.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://sportsapp.herokuapp.com/api/";
+export const API_URL = "http://sportsapp.herokuapp.com/api/";
+// export const API_URL = "http://127.0.0.1:8000/api/";
 export const AVATAR_URL = "";
